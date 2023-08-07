49eb1df4e5632359c91617aef17ce25296075e39
Gustavo <gucastiliao@gmail.com>
2023-08-07 06:31:41 -0300

    feat: Unixepoch.dev

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,55 @@
 import React from "react"
 
 class Footer extends React.Component {
-    render() {
-      return [
-        <footer>
-          <section className="links">
-            <p><a href="https://twitter.com/gustv0_" target="_blank" rel="noreferrer">Twitter</a></p>
-            <p><a href="https://www.linkedin.com/in/gucastiliao" target="_blank" rel="noreferrer">Linkedin</a></p>
-            <p><a href="https://github.com/gustv000" target="_blank" rel="noreferrer">GitHub</a></p>
-            <p><a href="https://twitter.com/quebradev" target="_blank" rel="noreferrer">QuebraDev</a></p>
-          </section>
-        </footer>
-      ]
-    }
+  render() {
+    return [
+      <footer>
+        <section className="links">
+          <p>
+            <a
+              href="https://twitter.com/gustv0_"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://www.linkedin.com/in/gucastiliao"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://github.com/gustv000"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://twitter.com/quebradev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              QuebraDev
+            </a>
+          </p>
+          <p>
+            <a href="https://unixepoch.dev" target="_blank" rel="noreferrer">
+              unixepoch.dev
+            </a>
+          </p>
+        </section>
+      </footer>,
+    ]
+  }
 }
 
 export default Footer
